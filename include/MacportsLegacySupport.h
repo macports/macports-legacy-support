a8b995dfae268802fdd5b949753b317585573eab
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2018-10-27 15:00:36 +0100

    fix clock_gettime check

--- include/MacportsLegacySupport.h
+++ include/MacportsLegacySupport.h
@@ -4,7 +4,7 @@
 // defines for when legacy support is required for various functions
 
 // clock_gettime
-#define __MP_LEGACY_SUPPORT_GETTIME__ __APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 101100
+#define __MP_LEGACY_SUPPORT_GETTIME__ __APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 101200
 
 // strnlen
 #define __MP_LEGACY_SUPPORT_STRNLEN__ __APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1070
