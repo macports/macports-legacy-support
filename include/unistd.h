6fe7cf0ad34aefd40760cee8baa2c63267938175
Ken Cunningham <kencu@macports.org>
2019-01-21 19:15:34 -0800

    "wrap" realpath and sysconf functions
    these changes "wrap" existing library functions
    modifying the behaviour to behave like current
    expected library functions
    
    realpath: leopard and earlier
    support NULL as buffer
    
    sysconf: tiger
    support _SC_NPROCESSORS_CONF and _SC_NPROCESSORS_ONLN

---
+++ include/unistd.h
@@ -0,0 +1,45 @@
+/* MP support header */
+#include "MacportsLegacySupport.h"
+
+
+#if __MP_LEGACY_SUPPORT_SYSCONF_WRAP__
+#ifndef __BUILDING_MP_LEGACY_SUPPORT_SYSCONF_WRAP__
+
+/* redefine the original sysconf */
+#undef sysconf
+#define sysconf sysconf_orig
+
+#endif /* __BUILDING_MP_LEGACY_SUPPORT_SYSCONF_WRAP__ */
+#endif /*__MP_LEGACY_SUPPORT_SYSCONF_WRAP__*/
+
+
+
+#include_next <unistd.h>
+
+
+
+#if __MP_LEGACY_SUPPORT_SYSCONF_WRAP__
+#ifndef __BUILDING_MP_LEGACY_SUPPORT_SYSCONF_WRAP__
+
+/* and now define sysconf as our new wrapped function */
+#undef sysconf
+#define sysconf macports_legacy_sysconf
+
+#ifndef _SC_NPROCESSORS_CONF
+#define _SC_NPROCESSORS_CONF 57
+#endif
+
+#ifndef _SC_NPROCESSORS_ONLN
+#define _SC_NPROCESSORS_ONLN 58
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern long macports_legacy_sysconf(int);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BUILDING_MP_LEGACY_SUPPORT_SYSCONF_WRAP__ */
+#endif /*__MP_LEGACY_SUPPORT_SYSCONF_WRAP__*/
