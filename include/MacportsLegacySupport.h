91d81497986d2c1112786052521ab0aae7d2f2d4
Ken Cunningham <kencu@macports.org>
2019-01-31 17:46:54 -0800

    avoid Wexpansion-to-defined on clang
    this should keep all our compilers happy...

--- include/MacportsLegacySupport.h
+++ include/MacportsLegacySupport.h
@@ -60,16 +60,21 @@
 #define __MP_LEGACY_SUPPORT_POSIX_MEMALIGN__  (__APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1060)
 
 /*  realpath() wrap */
-#define __MP_LEGACY_SUPPORT_REALPATH_WRAP__   (__APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1060      \
-                                                         && !defined (__DISABLE_MP_LEGACY_SUPPORT_REALPATH_WRAP__)   \
+#if                                           (__APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1060      \
+                                                         && !defined (__DISABLE_MP_LEGACY_SUPPORT_REALPATH_WRAP__)    \
                                                          && !defined (__DISABLE_ALL_MACPORTS_FUNCTION_WRAPPING__) )
+# define __MP_LEGACY_SUPPORT_REALPATH_WRAP__
+#endif
+
 /* lsmod */
 #define __MP_LEGACY_SUPPORT_LSMOD__           (__APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050)
 
 /*  sysconf() wrap */
-#define __MP_LEGACY_SUPPORT_SYSCONF_WRAP__    (__APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050      \
-                                                         && !defined (__DISABLE_MP_LEGACY_SUPPORT_SYSCONF_WRAP__ )   \
+#if                                           (__APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1050      \
+                                                         && !defined (__DISABLE_MP_LEGACY_SUPPORT_SYSCONF_WRAP__ )    \
                                                          && !defined (__DISABLE_ALL_MACPORTS_FUNCTION_WRAPPING__) )
+# define __MP_LEGACY_SUPPORT_SYSCONF_WRAP__
+#endif
 
 /* arc4random */
 #define __MP_LEGACY_SUPPORT_ARC4RANDOM__      (__APPLE__ && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 1070)
