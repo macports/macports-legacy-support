83845e4b2b8a312a90693d4d23360fc9dff0a081
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2019-08-15 14:57:48 +0100

    sincos.c fix copyright

--- src/sincos.c
+++ src/sincos.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2010 Chris Jones <jonesc@macports.org>
- * Copyright (c) 2019 Michael Dickens <michaelld@macports.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
