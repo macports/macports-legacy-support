0d92cacc256e880d3c403adf0c327c771aab9c5f
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2020-05-26 09:38:20 +0100

    fix copyright dates

--- test/test_ffsl.c
+++ test/test_ffsl.c
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2019 Chris Jones
+ * Copyright (c) 2020 Chris Jones
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
