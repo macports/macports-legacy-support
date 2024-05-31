4405385bb024205da873fb3b4aabf9099b1cd5be
Fred Wright <fw@fwright.net>
2024-05-30 18:26:43 -0700

    Move renameat() to the proper header.
    Commit e9cf5ab3 moved the renameat() declaration from sys/stdio.h to
    stdio.h, but this is incompatible with the normal header setup.
    This moves it back to restore compatibility, but requires knowledge
    of the SDK version to cope with the absence of sys/stdio.h in
    pre-10.10 SDKs.  This is handled via the new sdkversion.h.
    
    Re: https://trac.macports.org/ticket/69867
    
    TESTED:
    Builds and passes tests on all platforms, including the new test for
    this issue.

---
+++ include/sys/stdio.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2010 Chris Jones <jonesc@macports.org>
+ * Copyright (c) 2018 Ken Cunningham <kencu@macports.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef _MACPORTS_SYS_STDIO_H_
+#define _MACPORTS_SYS_STDIO_H_
+
+/* MP support header */
+#include "MacportsLegacySupport.h"
+
+/* Do our SDK-related setup */
+#include <_macports_extras/sdkversion.h>
+
+/* Include the primary system sys/stdio.h (10.7+ only) */
+#if !__MPLS_PRE_10_10_SDK
+#include_next <sys/stdio.h>
+#endif
+
+#if __MP_LEGACY_SUPPORT_ATCALLS__
+
+/* renameat */
+__MP__BEGIN_DECLS
+extern int renameat(int olddirfd, const char *oldpath,
+                    int newdirfd, const char *newpath);
+__MP__END_DECLS
+
+#endif /* __MP_LEGACY_SUPPORT_ATCALLS__ */
+
+#endif /* _MACPORTS_SYS_STDIO_H_ */
