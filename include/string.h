87bd0675fafefc211514385732a503e44715828a
Fred Wright <fw@fwright.net>
2024-06-24 18:16:21 -0700

    Use proper SDK/lib conditions for stpncpy().
    This splits the feature flag for stpncpy() into separate flags for the
    target OS version and the SDK version (the first of many such cases).
    For the library, this just renames the feature flag.  For the header,
    it's complicated by the added feature of fixing the possible
    incompatibility between 10.7+ SDKs and old compilers.  See the
    comments in string.h.
    
    TESTED:
    Tests pass on all platforms, including mismatched SDK cases.

--- include/string.h
+++ include/string.h
@@ -20,16 +20,18 @@
 /* MP support header */
 #include "MacportsLegacySupport.h"
 
+/* Do our SDK-related setup */
+#include <_macports_extras/sdkversion.h>
+
 /* Include the primary system string.h */
 #include_next <string.h>
 
 /* stpncpy */
-#if __MP_LEGACY_SUPPORT_STPNCPY__
+#if __MPLS_SDK_SUPPORT_STPNCPY__
 __MP__BEGIN_DECLS
-#undef stpncpy  /* In case built with later SDK */
 extern char *stpncpy(char *dst, const char *src, size_t n);
 __MP__END_DECLS
-#endif /* __MP_LEGACY_SUPPORT_STPNCPY__ */
+#endif /* __MPLS_SDK_SUPPORT_STPNCPY__ */
 
 /* strnlen */
 #if __MP_LEGACY_SUPPORT_STRNLEN__
@@ -66,15 +68,19 @@ __MP__END_DECLS
 
 /* stpncpy */
 
-#if __MP_LEGACY_SUPPORT_STPNCPY__
 /*
+ * Ordinarily this would just use the usual SDK conditional.  But the
+ * __builtin___stpncpy_chk workaround is useful even when the security
+ * wrapper is supplied by the SDK, so the condition for supplying our own
+ * wrapper is more complicated.
+ *
  * Some compilers lack __builtin___stpncpy_chk, requiring a workaround.
  * Handling this is complicated by the fact that support was added to
  * some compilers prior to the __has_builtin() feature that allows directly
  * testing for it.  It's possible to work around this based on the compiler
  * version, but that's further complicated by the fact that Apple added it
  * earlier than the official GCC addition in 4.7.  So, in the absence of
- * a positive __has_builtin() result, we need to see whether either the
+ * the availability of __has_builtin(), we need to see whether either the
  * GCC version or the Apple GCC version is sufficiently recent.
  *
  * Apple didn't get around to adding this logic until the 10.9 SDK, even
@@ -90,6 +96,8 @@ __MP__END_DECLS
  * in the 10.7+ SDK case, we use a different name for the inline, which
  * also avoids a duplicate definition issue.
  */
+
+/* Create a proper test for the builtin */
 #ifdef __has_builtin
 #define __HAVE_BUILTIN_STPNCPY_CHK__ __has_builtin(__builtin___stpncpy_chk)
 #else
@@ -98,13 +106,17 @@ __MP__END_DECLS
          || __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7))
 #endif
 
+/* Define workaround if needed */
 #if !__HAVE_BUILTIN_STPNCPY_CHK__
 extern char *__stpncpy_chk(char *dest, const char *src, size_t len,
                            size_t dstlen);
 #define __builtin___stpncpy_chk __stpncpy_chk
+#undef stpncpy
 #endif /* !__HAVE_BUILTIN_STPNCPY_CHK__ */
 
-#undef stpncpy
+#if __MPLS_SDK_SUPPORT_STPNCPY__ || !__HAVE_BUILTIN_STPNCPY_CHK__
+
+/* Define the wrapper, possibly replacing the SDK version */
 #define stpncpy(dest, src, len)					\
   ((__darwin_obsz0 (dest) != (size_t) -1)				\
    ? __builtin___stpncpy_chk (dest, src, len, __darwin_obsz (dest))	\
@@ -117,7 +129,7 @@ __mpls_inline_stpncpy_chk (char *__restrict __dest,
   return __builtin___stpncpy_chk (__dest, __src, __len, __darwin_obsz(__dest));
 }
 
-#endif /* __MP_LEGACY_SUPPORT_STPNCPY__ */
+#endif /* __MPLS_SDK_SUPPORT_STPNCPY__ || !__HAVE_BUILTIN_STPNCPY_CHK__ */
 
 #endif /* _USE_FORTIFY_LEVEL > 0 */
 
