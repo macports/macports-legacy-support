866a0807191148098974d0f502a9409b49fb46e0
Fred Wright <fw@fwright.net>
2024-09-25 15:20:05 -0700

    Add scandir() compatibility feature.
    This adds a wrapper to make the scandir() 'compar' argument and the
    provided alphasort() function match the types used in 10.8+, thereby
    avoiding warnings, and in some cases errors, due to the pointer type
    mismatch.
    
    To avoid breaking code expecting the old behavior, this hack is only
    applied when _MACPORTS_LEGACY_COMPATIBLE_SCANDIR is defined nonzero.
    
    Closes: https://trac.macports.org/ticket/70702
    
    TESTED:
    Tests pass on all OS versions with this fix.

--- include/dirent.h
+++ include/dirent.h
@@ -46,6 +46,62 @@ __MP__END_DECLS
 
 #endif /* __MPLS_SDK_SUPPORT_FDOPENDIR__ */
 
+/* new signature for scandir and alphasort (optionally) */
+#if __MPLS_SDK_SUPPORT_NEW_SCANDIR__ \
+    && defined(_MACPORTS_LEGACY_COMPATIBLE_SCANDIR) \
+    && _MACPORTS_LEGACY_COMPATIBLE_SCANDIR
+
+/*
+ * Here we provide compatibility wrappers so that scandir and alphasort
+ * references based on the new function signatures can be used with the
+ * old versions of the functions.  To avoid breaking code expecting the
+ * normal behavior, these wrappers are only introduced conditionally,
+ * based on _MACPORTS_LEGACY_COMPATIBLE_SCANDIR being nonzero.
+ *
+ * The wrapper functions are defined as inlines, and then macros are
+ * defined to make them replace the normal functions.  Since the sole
+ * effect of the wrappers is to apply some casts for pointer-type
+ * compatibility, it's unlikely that any actual executable code is added.
+ * As inline functions, it's still legal to take pointers to them, in
+ * which case the compiler will generate local callable instances.
+ *
+ * The underlying functions have the DARWIN_ALIAS treatment, to handle
+ * optional INODE64 behavior.  The inline functions don't need that, since
+ * they simply wrap the calls to the underlying functions, which are then
+ * directed as appropriate.  In the function pointer case, any local
+ * callable instance is private to the compilation unit, and need only
+ * handle the INODE64 case in effect there.
+ */
+
+__MP__BEGIN_DECLS
+
+static inline int
+__mpls_alphasort(const struct dirent **d1, const struct dirent **d2)
+{
+  /*
+   * The casts here are currently unnecessary, but could be needed in
+   * the future if compilers get pickier.
+   */
+  return alphasort((const void *) d1, (const void *) d2);
+}
+
+static inline int
+__mpls_scandir(const char *dirname, struct dirent ***namelist,
+               int (*select)(const struct dirent *),
+               int (*compar)(const struct dirent **, const struct dirent **))
+{
+  return scandir(dirname, namelist,
+                 (int (*)(struct dirent *)) select,
+                 (int (*)(const void *, const void *)) compar);
+}
+
+__MP__END_DECLS
+
+#define alphasort __mpls_alphasort
+#define scandir __mpls_scandir
+
+#endif /* __MPLS_SDK_SUPPORT_NEW_SCANDIR__ && ... */
+
 #endif /* __DARWIN_C_LEVEL >= 200809L */
 
 #endif /* _MACPORTS_DIRENT_H_ */
