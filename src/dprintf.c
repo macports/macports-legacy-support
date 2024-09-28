61fec98cb5078c9a36749bd1675ebe427aca11bb
Fred Wright <fw@fwright.net>
2024-09-27 20:06:37 -0700

    Rewrite dprintf() implementation.
    This is primarily to fix a bug in dprintf() which erroneously closed
    the fd after its use, but is also mildly more efficient.
    
    Based on some work by raf <raf@raf.org>
    
    TESTED:
    Passes the updated test, which failed before this fix.

--- src/dprintf.c
+++ src/dprintf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Evan Miller   <emmiller@gmail.com>
+ * Copyright (c) 2024 Frederick H. G. Wright II <fw@fwright.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,26 +14,45 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-// MP support header
+/* MP support header */
 #include "MacportsLegacySupport.h"
 #if __MPLS_LIB_SUPPORT_DPRINTF__
 
+#include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <sys/errno.h>
+#include <unistd.h>
 
-int dprintf(int fildes, const char * __restrict format, ...) {
-    FILE *stream = fdopen(fildes, "w");
-    if (stream == NULL) {
-        errno = EBADF;
-        return -1;
-    }
-    va_list ap;
-    va_start(ap, format);
-    int result = vfprintf(stream, format, ap);
-    va_end(ap);
-    fclose(stream);
-    return result;
+int
+dprintf(int fildes, const char * __restrict format, ...) {
+  va_list ap;
+  FILE *stream;
+  int ret;
+  char buf[BUFSIZ];
+
+  /* Create a stream for a copy of the target fd, with our local buffer. */
+  stream = fdopen(dup(fildes), "w");
+  if (stream == NULL) {
+    errno = EBADF;  /* Set the expected errno if it fails */
+    return -1;
+  }
+  setbuffer(stream, buf, sizeof(buf));
+
+  /* Do the output. */
+  va_start(ap, format);
+  ret = vfprintf(stream, format, ap);
+  va_end(ap);
+
+  /*
+   * Close the FILE and the duplicate fd.
+   *
+   * NOTE: This releases any locks held by the original fd.  There doesn't
+   * seem to be an easy way to avoid this, given that free(stream) doesn't
+   * work.
+   */
+  if (fclose(stream)) ret = -1;
+
+  return ret;
 }
 
 #endif /* __MPLS_LIB_SUPPORT_DPRINTF__ */
