b70895d153a4e5074f215a4174662d0ec3ebd599
Fred Wright <fw@fwright.net>
2024-11-06 17:44:18 -0800

    test_realpath: Add test for legacy compatibility entry.
    This tests the macports_legacy_realpath() entry point, which is no
    longer used by new builds but possibly referenced by old builds.
    
    TESTED:
    Passes on all platforms; fails when the legacy entry is disabled.

---
+++ test/test_realpath_compat.c
@@ -0,0 +1,25 @@
+/*
+ * Version of test_realpath for compatibility entry.
+ */
+
+/*
+ * This tests the macports_legacy_realpath() compatibility entry.
+ *
+ * For OS versions that never used the wrapper, this entire test is a dummy.
+ * Because of that condition, this test is an exception to the rule that tests
+ * shouldn't check the feature flags.
+ */
+
+/* MP support header */
+#include "MacportsLegacySupport.h"
+#if __MPLS_LIB_SUPPORT_REALPATH_WRAP__
+
+#define TEST_MACPORTS_LEGACY_REALPATH
+
+#include "test_realpath.c"
+
+#else /* !__MPLS_LIB_SUPPORT_REALPATH_WRAP__ */
+
+int main(){ return 0; }
+
+#endif /* !__MPLS_LIB_SUPPORT_REALPATH_WRAP__ */
