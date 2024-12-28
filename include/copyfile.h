60a940dd50224d2b2061f47ba2b91e4b40ff2bd7
Fred Wright <fw@fwright.net>
2024-12-27 16:51:28 -0800

    Rework 10.4 copyfile headers.
    This reworks the arrangement of the copyfile-related headers for 10.4,
    where the SDK provided none.  In the new arrangement:
    
    1) The added header now represents what a 10.4 copyfile.h would have
    looked like if it existed.
    
    2) The additions for 10.4->10.5 copyfile are now in the wrapper
    header.
    
    3) A new pair of feature flags is added for the 10.4 case.
    
    TESTED:
    Library still builds.
    Test builds, and passes or fails as before.

--- include/copyfile.h
+++ include/copyfile.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022
+ * Copyright (c) 2024
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -23,24 +23,67 @@
 /* Do our SDK-related setup */
 #include <_macports_extras/sdkversion.h>
 
-#if __MPLS_SDK_MAJOR < 1050
+#if __MPLS_SDK_SUPPORT_COPYFILE_TIGER__
 
+/* The 10.4 SDK lacks copyfile.h, so we provide a substitute. */
 #include <_macports_extras/tiger_only/copyfile.h>
 
-/* The replacement copyfile.h doesn't need the wrapper */
+/* Additional defs from the 10.5 copyfile.h */
 
-#else /* __MPLS_SDK_MAJOR >= 1050 */
+__MP__BEGIN_DECLS
 
-/* Include the primary system copyfile.h */
+int fcopyfile(int from_fd, int to_fd, copyfile_state_t, copyfile_flags_t flags);
+
+int copyfile_state_free(copyfile_state_t);
+copyfile_state_t copyfile_state_alloc(void);
+
+int copyfile_state_get(copyfile_state_t s, uint32_t flag, void * dst);
+int copyfile_state_set(copyfile_state_t s, uint32_t flag, const void * src);
+
+__MP__END_DECLS
+
+#define COPYFILE_STATE_SRC_FD		1
+#define COPYFILE_STATE_SRC_FILENAME	2
+#define COPYFILE_STATE_DST_FD		3
+#define COPYFILE_STATE_DST_FILENAME	4
+#define COPYFILE_STATE_QUARANTINE	5
+
+#undef COPYFILE_DISABLE_VAR
+#define	COPYFILE_DISABLE_VAR	"COPYFILE_DISABLE"
+
+#else /* !__MPLS_SDK_SUPPORT_COPYFILE_TIGER__ */
+
+/* Otherwise include the primary system copyfile.h */
 #include_next <copyfile.h>
 
-#endif /* __MPLS_SDK_MAJOR >= 1050 */
+#endif /* !__MPLS_SDK_SUPPORT_COPYFILE_TIGER__ */
 
 #if __MPLS_SDK_SUPPORT_COPYFILE_WRAP__
+
+/* Additional defs from the 10.6 copyfile.h */
+
+typedef int (*copyfile_callback_t)(int, int, copyfile_state_t, const char *, const char *, void *);
 
 #define COPYFILE_STATE_STATUS_CB        6
 #define COPYFILE_STATE_STATUS_CTX       7
 #define COPYFILE_STATE_COPIED           8
+
+#define	COPYFILE_RECURSIVE	(1<<15)	/* Descend into hierarchies */
+
+#define	COPYFILE_RECURSE_ERROR	0
+#define	COPYFILE_RECURSE_FILE	1
+#define	COPYFILE_RECURSE_DIR	2
+#define	COPYFILE_RECURSE_DIR_CLEANUP	3
+#define	COPYFILE_COPY_DATA	4
+
+#define	COPYFILE_START		1
+#define	COPYFILE_FINISH		2
+#define	COPYFILE_ERR		3
+#define	COPYFILE_PROGRESS	4
+
+#define	COPYFILE_CONTINUE	0
+#define	COPYFILE_SKIP	1
+#define	COPYFILE_QUIT	2
 
 #endif /* __MPLS_SDK_SUPPORT_COPYFILE_WRAP__ */
 
