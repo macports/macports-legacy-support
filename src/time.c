f158c391ade364fe515f7f7cc471a994419407e6
Fred Wright <fw@fwright.net>
2025-01-25 22:08:33 -0800

    Rewrite clock_gettime_nsec_np()
    This rewrites clock_gettime_nsec_np() as a separate function, rather
    than as a wrapper around clock_gettime().  It does not redo the
    mach_time scaling, which is left for another commit.  It does
    provide some groundwork for improvements to clock_gettime().
    
    Also adds the missing "sandwich" to get a consistent
    boottime/timeofday pair, to avoid a race.
    
    TESTED:
    Passes new clock test on all platforms, with temporary forgiveness for
    inaccurate mach_time scaling on PPC.

--- src/time.c
+++ src/time.c
@@ -36,13 +36,119 @@
 #define BILLION32 1000000000U
 #define BILLION64 1000000000ULL
 
-/* Interim implementation of clock_gettime_nsec_np() as wrapper */
+/*
+ * Get the system boot time.  The faster means of doing this via a
+ * communication page wasn't introduced until 10.12, where this code is
+ * inapplicable, so we're stuck with doing it the slow way.
+ *
+ * Apple's similar code uses sysctlbyname(), which not only is slower
+ * but also doesn't work on 10.4.
+ */
+static int
+get_boottime(struct timeval *bt)
+{
+  size_t boottime_len = sizeof(*bt);
+  int bt_mib[] = {CTL_KERN, KERN_BOOTTIME};
+  size_t bt_miblen = sizeof(bt_mib) / sizeof(bt_mib[0]);
+
+  return sysctl(bt_mib, bt_miblen, bt, &boottime_len, NULL, 0);
+}
+
+/* Get a consistent boot time / time of day pair. */
+static int
+get_boot_and_tod(struct timeval *bt, struct timeval *tod)
+{
+  int ret;
+  /*
+   * Note that older systems that only have one-second resolution on boottime
+   * don't store the tv_usec field at all, making initialization mandatory.
+   * Otherwise, infinite loops may result.
+   */
+  struct timeval tv1 = {0, 0}, tv2 = {0, 0};
+
+  do {
+    if ((ret = get_boottime(&tv1))) return ret;
+    if ((ret = gettimeofday(tod, NULL))) return ret;
+    if ((ret = get_boottime(&tv2))) return ret;
+  } while (tv1.tv_sec != tv2.tv_sec || tv1.tv_usec != tv2.tv_usec);
+  *bt = tv1;
+  return 0;
+}
+
+/* Get the CPU usage of the current thread into user/system timevals. */
+static int
+get_thread_usage(time_value_t *ut, time_value_t *st)
+{
+  int ret;
+  mach_msg_type_number_t count = THREAD_BASIC_INFO_COUNT;
+  thread_basic_info_data_t info;
+
+  thread_port_t thread = mach_thread_self();
+  ret = thread_info(thread, THREAD_BASIC_INFO, (thread_info_t) &info, &count);
+  mach_port_deallocate(mach_task_self(), thread);
+  if (ret) return ret;
+
+  *ut = info.user_time;
+  *st = info.system_time;
+  return 0;
+}
+
 uint64_t
 clock_gettime_nsec_np(clockid_t clk_id)
 {
-  struct timespec ts;
-  if (clock_gettime(clk_id, &ts)) return 0;
-  return ts.tv_sec * BILLION64 + ts.tv_nsec;
+  uint64_t mach_time;
+  struct timeval tod, bt;
+  struct rusage ru;
+  time_value_t ut, st;
+  static mach_timebase_info_data_t tbinfo;
+
+  switch (clk_id) {
+
+  case CLOCK_REALTIME:
+    if (gettimeofday(&tod, NULL)) return 0;
+    return tod.tv_sec * BILLION64 + tod.tv_usec * 1000;
+
+  case CLOCK_MONOTONIC:
+    if (get_boot_and_tod(&bt, &tod)) return 0;
+    return (tod.tv_sec - bt.tv_sec) * BILLION64
+           + (tod.tv_usec - bt.tv_usec) * 1000;
+
+  case CLOCK_PROCESS_CPUTIME_ID:
+    if (getrusage(RUSAGE_SELF, &ru)) return 0;
+    return (ru.ru_utime.tv_sec + ru.ru_stime.tv_sec) * BILLION64
+           + (ru.ru_utime.tv_usec + ru.ru_stime.tv_usec) * 1000;
+
+  case CLOCK_THREAD_CPUTIME_ID:
+    if (get_thread_usage(&ut, &st)) return 0;
+    return (ut.seconds + st.seconds) * BILLION64
+           + (ut.microseconds + st.microseconds) * 1000;
+
+  /* For now these are all the same, matching CLOCK_UPTIME_RAW */
+  case CLOCK_MONOTONIC_RAW:
+  case CLOCK_MONOTONIC_RAW_APPROX:
+  case CLOCK_UPTIME_RAW:
+  case CLOCK_UPTIME_RAW_APPROX:
+    mach_time = mach_absolute_time();
+    break;
+
+  default:
+    errno = EINVAL;
+    return 0;
+  }
+
+  /* Obtain and cache mach_time scale factor (as a rational) */
+  if (!tbinfo.numer || !tbinfo.denom) {
+    if (mach_timebase_info(&tbinfo)) return 0;
+  }
+
+  /* Scale mach_time to nanoseconds and return it */
+
+  /* Note that 1/1 is a common case worth special-casing */
+  if (tbinfo.numer == tbinfo.denom) return mach_time;
+
+  /* Temporary low-accuracy conversion */
+  /* Multiplying first overflows on some old platforms */
+  return mach_time * (tbinfo.numer / tbinfo.denom);
 }
 
 int clock_gettime( clockid_t clk_id, struct timespec *ts )
