f0b91d14cdc085679fcbeee5946bc2d60d7cc7b3
Fred Wright <fw@fwright.net>
2025-02-21 22:28:23 -0800

    Add mach_approximate_time() and derivatives (10.10+).
    This makes mach_approximate_time() available prior to its addition in
    10.10, by falling back to mach_absolute_time().  The cases of
    clock_gettime[_nsec_np]() that use the approximate versions are also
    updated to use it, which has no effect prior to 10.10, but uses the
    approximate versons in 10.10+.
    
    TESTED:
    Passes new clock test on all platforms.

---
+++ include/mach/mach_time.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Frederick H. G. Wright II <fw@fwright.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef _MACPORTS_MACH_TIME_H_
+#define _MACPORTS_MACH_TIME_H_
+
+/* MP support header */
+#include "MacportsLegacySupport.h"
+
+/* Do our SDK-related setup */
+#include <_macports_extras/sdkversion.h>
+
+/* Include the primary system mach/mach_time.h */
+#include_next <mach/mach_time.h>
+
+#if __MPLS_SDK_SUPPORT_APPROX_TIME__
+
+__MP__BEGIN_DECLS
+
+uint64_t mach_approximate_time(void);
+
+__MP__END_DECLS
+
+#endif /* __MPLS_SDK_SUPPORT_APPROX_TIME__ */
+
+#endif /* _MACPORTS_MACH_TIME_H_ */
