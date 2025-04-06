3e60c321a69b31882ac760b96c72be71fc6ee7bf
Fred Wright <fw@fwright.net>
2025-04-06 12:40:54 -0700

    Make security-wrapper tests manual.
    Since the st?chk security-wrapper tests need to deliberately use
    illegal C code to test the checks, and since some compilers generate
    unsuppressible warnings or even errors in such cases, we move them to
    the manual category to avoid polluting the normal test suite.
    
    TESTED:
    Tests now run under their mantest names.

---
+++ manual_tests/strncpy_chk_force0.c
@@ -0,0 +1,7 @@
+/*
+ * Version of strncpy_chk with enable forced to 0 (to disable checks).
+ */
+
+#define _FORTIFY_SOURCE 0
+
+#include "strncpy_chk.c"
--- test/test_strncpy_chk_force0.c
+++
@@ -1,7 +0,0 @@
-/*
- * Version of test_strncpy_chk with enable forced to 0 (to disable checks).
- */
-
-#define _FORTIFY_SOURCE 0
-
-#include "test_strncpy_chk.c"
