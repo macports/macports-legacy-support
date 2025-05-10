71534f3fdfd423d3971b8a5ab74f81c4e9e63852
Fred Wright <fw@fwright.net>
2025-05-09 19:30:04 -0700

    Availability[Internal].h: Avoid lingering dummy __has*().
    The leftover default dummy definition of __has_include() was preventing
    the special workaround in MacTypes.h from working correctly.  This
    removes both dummy __has*() definitions after their use.
    
    However, Availability.h was relying on these leftover defaults to
    function correctly, so we add similar logic to its wrapper.
    
    Both of these have now gotten complicated enough to justify guard macros.
    
    TESTED:
    Passes all tests, including new	allheaders tests.

--- include/Availability.h
+++ include/Availability.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Frederick H. G. Wright II <fw@fwright.net>
+ * Copyright (c) 2025 Frederick H. G. Wright II <fw@fwright.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,14 +14,30 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifndef _MACPORTS_AVAILABILITY_H_
+#define _MACPORTS_AVAILABILITY_H_
+
 /*
  * This is a wrapper header for Availability.h, to handle its absence in
  * the 10.4 SDK.  In that case, we provide a substitute; otherwise we
  * just pass through the SDK header.
  *
- * We don't bother with a guard macro, since all we do here is include
- * other headers which have their own guard macros, and we don't define
- * anything here.
+ * We also provide a dummy definition of __has_include() when the compiler
+ * doesn't provide it and we're using a 10.14+ SDK, which uses it here
+ * without checking.  It would probably be OK to do this for all SDKs,
+ * but for safety we limit it to the relevant cases.  Note that this has
+ * to be done *before* the include_next.
+ *
+ * A similar issue exists with __has_builtin() in the 14.x+ SDK.  In this
+ * case, the offending code attempts to handle the missing feature by
+ * including a defined() condition, but that doesn't actually work because
+ * the intrinsic needs to be parseable before evaluating the boolean.
+ * So we again provide a default when needed.
+ *
+ * For both of the above, we only temporarily provide the dummy definitions
+ * during the include_next, to avoid confusing other includes, such as the
+ * __has_include() mess in MacTypes.h.  We use header-specific flag macros
+ * for this, to avoid nesting issues.
  *
  * There'a complication in the case where AvailabilityMacros.h (included by
  * sdkversion.h) #includes Availability.h (currently in 10.9+ SDKs).  In that
@@ -35,8 +51,34 @@
 /* Do our SDK-related setup */
 #include <_macports_extras/sdkversion.h>
 
+#ifdef __MPLS_SDK_MAJOR
+
+#if __MPLS_SDK_MAJOR >= 101400 && !defined(__has_include)
+#define __has_include(x) 0
+#define __MPLS_DUMMY_HAS_INCLUDE_AVAIL
+#endif
+
+#if __MPLS_SDK_MAJOR >= 140000 && !defined(__has_builtin)
+#define __has_builtin(x) 0
+#define __MPLS_DUMMY_HAS_BUILTIN_AVAIL
+#endif
+
+#endif /* __MPLS_SDK_MAJOR */
+
 #if defined(__MPLS_SDK_MAJOR) && __MPLS_SDK_MAJOR < 1050
 #include <_macports_extras/tiger_only/Availability.h>
 #else
 #include_next <Availability.h>
 #endif
+
+#ifdef __MPLS_DUMMY_HAS_INCLUDE_AVAIL
+#undef __has_include
+#undef __MPLS_DUMMY_HAS_INCLUDE_AVAIL
+#endif
+
+#ifdef __MPLS_DUMMY_HAS_BUILTIN_AVAIL
+#undef __has_include
+#undef __MPLS_DUMMY_HAS_BUILTIN_AVAIL
+#endif
+
+#endif /* _MACPORTS_AVAILABILITY_H_ */
