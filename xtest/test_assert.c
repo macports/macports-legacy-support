d297ee9a98ad1b81ec99b54a04ac17fbf5ec9120
Fred Wright <fw@fwright.net>
2025-05-12 23:21:17 -0700

    test_assert: Fix for C89/C90 build.
    TESTED:
    Builds properly for C89, C90, C99, and C11.

--- xtest/test_assert.c
+++ xtest/test_assert.c
@@ -6,12 +6,15 @@ main(int argc, char *argv[])
 {
   (void) argc; (void) argv;
 
-#if __STDC_VERSION__ >= 201100
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201100
   static_assert(1, "true");
-  printf("static_assert compiled successfully in C11 mode\n\n");
-#else
+  printf("static_assert compiled successfully in C11 mode\n");
+#elif defined(__STDC_VERSION__)
   printf("static_assert is unavailable before C11, __STDC_VERSION__ = %d\n",
          (int) __STDC_VERSION__);
+#else
+  printf("static_assert is unavailable before C11,"
+         " __STDC_VERSION__ is undefined\n");
 #endif
   return 0;
 }
