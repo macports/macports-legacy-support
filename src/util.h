02422662607d29e1769ee898e483a4e290a177d3
Fred Wright <fw@fwright.net>
2025-07-02 11:17:31 -0700

    util: New module for common utilities.
    Initially this just contains the access checker from fcntl.c.
    
    The checker is now improved to avoid an extra call in the single-page
    case, as well as handling multi-page ranges correctly.  It also now
    accepts an optional known-valid address, and avoids the full check
    if the target range lies within the known-valid page.
    
    TESTED:
    Tests pass on all platforms.

---
+++ src/util.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Frederick H. G. Wright II <fw@fwright.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/* This (conditionally) contains miscellaneous global utility features. */
+
+/* MP support header */
+#include "MacportsLegacySupport.h"
+
+#define __MPLS_NEED_CHECK_ACCESS__ __MPLS_LIB_FIX_TIGER_PPC64__
+
+#if __MPLS_NEED_CHECK_ACCESS__
+
+#include <mach/mach_vm.h>
+
+int __mpls_check_access(void *adr, mach_vm_size_t size, vm_prot_t access,
+                        void *okadr);
+
+#endif /* __MPLS_NEED_CHECK_ACCESS__ */
