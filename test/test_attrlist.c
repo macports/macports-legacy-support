aeefd4361e2469e55b67db60898518abd97092a5
Fred Wright <fw@fwright.net>
2025-07-12 18:55:12 -0700

    test_attrlist: Add timestamp tests.
    This optionally tests various combinations of timestamp setting, and
    illustrates the known OS bug related to setting atime alone on
    non-APFS filesystems.
    
    TESTED:
    Passes on all platforms by default.
    Optionally illustrates timestamp bugs.

--- test/test_attrlist.c
+++ test/test_attrlist.c
@@ -30,6 +30,7 @@
 #include <unistd.h>
 
 #include <sys/attr.h>
+#include <sys/mount.h>
 #include <sys/param.h>
 #include <sys/vnode.h>
 
@@ -47,7 +48,10 @@ typedef unsigned long attrlist_opts_t;
 #define FINFO_CREATOR "CRTR"
 #define FINFO_TCSIZE  4
 
+#define LL (long long)
+
 typedef struct attrlist attrlist_t;
+typedef struct timespec timespec_t;
 
 /* FinderInfo alone */
 typedef struct FInfoAttrBuf_s {
@@ -75,6 +79,49 @@ set_tc(FInfoAttrBuf_t *ab)
     return 0;
 }
 
+/*
+ * There is a long-standing OS bug (10.5 and later) where attempting to set
+ * the atime alone on an HFS+ (non-APFS?) file fails.  We include tests
+ * for setting timestamps to check this.
+ *
+ * For completeness, we test all possible combinations of timestamp setting.
+ * Also, since the "script" attribute comes earlier in the attribute sequence,
+ * and hence affects the buffer positions of the timestamps, we optionally
+ * "set" that as well, though without actually changing its value.  No other
+ * settable attributes appear earlier in the buffer than the timestamps.
+ *
+ * For each timestamp being set, we attempt to set it two seconds earlier
+ * than its current value.  We leave the nanoseconds alone, thereby avoiding
+ * issues related to whether the filesystem supports sub-second timestamps.
+ * Since there are a total of 31 cases, and each timestamp is changed in half
+ * of the cases, if everything worked correctly we'd see each timestamp
+ * moved backwards by 32 seconds.
+ */
+
+/* Attributes to test (in order of appearance) */
+#define ATTR_TESTS (ATTR_CMN_SCRIPT \
+                    | ATTR_CMN_CRTIME | ATTR_CMN_MODTIME \
+                    | ATTR_CMN_CHGTIME | ATTR_CMN_ACCTIME)
+
+/* Least-significant test bit (for incrementing) */
+#define ATTR_TESTS_LSB (ATTR_TESTS & (~ATTR_TESTS + 1))
+
+/* Test mask should be contiguous */
+#if (ATTR_TESTS + ATTR_TESTS_LSB) & ATTR_TESTS
+#error ATTR_TESTS is not contiguous
+#endif
+
+/* Structure for all timestamp-test attributes, including the (read) count */
+/* The initial two 32-bit items cause the timespecs to be naturally aligned. */
+typedef struct tsattr_s {
+    u_int32_t       length;
+    text_encoding_t script;
+    timespec_t      crtime;
+    timespec_t      modtime;
+    timespec_t      chgtime;
+    timespec_t      acctime;
+} tsattr_t;
+
 static int
 xgetattrlist(int mode, const char* path, int fd, attrlist_t *attrList,
              void *attrBuf, size_t attrBufSize, attrlist_opts_t options)
@@ -92,7 +139,147 @@ xsetattrlist(int mode, const char* path, int fd, attrlist_t *attrList,
 }
 
 static int
-do_tests(int mode, const char *path, int verbose)
+get_tsattrs(int mode, const char* path, int fd, tsattr_t *tsb)
+{
+  int ret;
+  attrlist_t al = {.bitmapcount = ATTR_BIT_MAP_COUNT,
+                   .commonattr = ATTR_TESTS};
+
+  ret = xgetattrlist(mode, path, fd, &al, tsb, sizeof(*tsb), 0);
+  if (ret) {
+    printf("      *** %sgetattrlist() for timestamps failed: %s\n",
+           mode ? "f" : "", strerror(errno));
+    return ret;
+  }
+  if (tsb->length != sizeof(*tsb)) {
+    printf("      *** %sgetattrlist() for timestamps returned"
+           " length %d, should be %d\n",
+           mode ? "f" : "", tsb->length, (int) sizeof(*tsb));
+  }
+  return 0;
+}
+
+static int
+set_tsattrs(int mode, const char* path, int fd, tsattr_t *tsb,
+            attrgroup_t attrs)
+{
+  int ret;
+  attrlist_t al = {.bitmapcount = ATTR_BIT_MAP_COUNT,
+                   .commonattr = attrs};
+  uint8_t wbuf[sizeof(*tsb)], *wbp = &wbuf[0];
+
+  if (attrs & ATTR_CMN_SCRIPT) {
+    *((text_encoding_t *) wbp) = tsb->script;
+    wbp += sizeof(text_encoding_t);
+  }
+  if (attrs & ATTR_CMN_CRTIME) {
+    tsb->crtime.tv_sec -= 2;
+    *((timespec_t *) wbp) = tsb->crtime;
+    wbp += sizeof(timespec_t);
+  }
+  if (attrs & ATTR_CMN_MODTIME) {
+    tsb->modtime.tv_sec -= 2;
+    *((timespec_t *) wbp) = tsb->modtime;
+    wbp += sizeof(timespec_t);
+  }
+  if (attrs & ATTR_CMN_CHGTIME) {
+    tsb->chgtime.tv_sec -= 2;
+    *((timespec_t *) wbp) = tsb->chgtime;
+    wbp += sizeof(timespec_t);
+  }
+  if (attrs & ATTR_CMN_ACCTIME) {
+    tsb->acctime.tv_sec -= 2;
+    *((timespec_t *) wbp) = tsb->acctime;
+    wbp += sizeof(timespec_t);
+  }
+
+  ret = xsetattrlist(mode, path, fd, &al, &wbuf, wbp - &wbuf[0], 0);
+  if (ret) {
+    printf("      *** %ssetattrlist() for timestamps failed: %s\n",
+           mode ? "f" : "", strerror(errno));
+    return ret;
+  }
+  return 0;
+}
+
+static int
+check_ts(timespec_t *out, timespec_t *in,
+         const char *name, attrgroup_t bit, attrgroup_t mask)
+{
+  if (in->tv_sec != out->tv_sec || in->tv_nsec != out->tv_nsec) {
+    printf("      *** %s (%sset) mismatches,"
+           " sent = %lld.%09lld, rcvd = %lld.%09lld, mask = %04X\n",
+           name, mask & bit ? "" : "not ",
+           LL out->tv_sec, LL out->tv_nsec, LL in->tv_sec, LL in->tv_nsec,
+           mask);
+    return 1;
+  }
+  return 0;
+}
+
+static int
+check_tsattrs(tsattr_t *out, tsattr_t *in, attrgroup_t attrs,
+              int verbose, int testmode)
+{
+  int ret = 0;
+
+  if (in->script != out->script) {
+    printf("      *** SCRIPT (%sset) mismatches,"
+           " sent = %u, rcvd = %u, mask = %04X\n",
+           attrs &ATTR_CMN_SCRIPT ? "" : "un",
+           out->script, in->script, attrs);
+    ret = 1;
+  }
+
+  ret |= check_ts(&out->crtime, &in->crtime,
+                  "CRTIME", ATTR_CMN_CRTIME, attrs);
+  ret |= check_ts(&out->modtime, &in->modtime,
+                  "MODTIME", ATTR_CMN_MODTIME, attrs);
+  if (testmode > 2) {  /* Setting CHGTIME is unsupported, even for root */
+    ret |= check_ts(&out->chgtime, &in->chgtime,
+                    "CHGTIME", ATTR_CMN_CHGTIME, attrs);
+  }
+  ret |= check_ts(&out->acctime, &in->acctime,
+                  "ACCTIME", ATTR_CMN_ACCTIME, attrs);
+
+  return ret;
+}
+
+static int
+do_tstests(int mode, const char *path, int fd, int apfs,
+           int verbose, int testmode)
+{
+  int ret = 0, err;
+  attrgroup_t mask;
+  tsattr_t tsb1, tsb2;
+
+  if (verbose) printf("    testing timestamps\n");
+  if ((err = get_tsattrs(mode, path, fd, &tsb1))) return err;
+
+  for (mask = ATTR_TESTS_LSB; mask & ATTR_TESTS; mask += ATTR_TESTS_LSB) {
+    if (verbose > 1) printf("      testing case %04X\n", mask);
+    if ((err = set_tsattrs(mode, path, fd, &tsb1, mask))) return err;
+    if ((err = get_tsattrs(mode, path, fd, &tsb2))) return err;
+    if ((err = check_tsattrs(&tsb1, &tsb2, mask, verbose, testmode))) {
+      /* Setting atime alone on non-APFS doesn't work */
+      /* On 10.11+, it also fails if attempting to set ctime as well */
+      if (!apfs && testmode < 2
+          && (mask == ATTR_CMN_ACCTIME
+              || mask == (ATTR_CMN_ACCTIME | ATTR_CMN_CHGTIME))) {
+        printf("        *** tolerating known OS bug\n");
+      } else {
+        ret |= err;
+      }
+    }
+    tsb1 = tsb2;
+  }
+
+  return ret;
+}
+
+static int
+do_tests(int mode, const char *path, int apfs,
+         int verbose, int testmode)
 {
   int ret = 0, fd = -1, err;
   attrlist_t al = {.bitmapcount = ATTR_BIT_MAP_COUNT};
@@ -221,6 +408,8 @@ do_tests(int mode, const char *path, int verbose)
              set, path, strerror(errno));
       ret = 1;
     }
+
+    if (testmode) ret |= do_tstests(mode, path, fd, apfs, verbose, testmode);
   } while (0);
 
   if (fd >= 0) (void) close(fd);
@@ -237,19 +426,40 @@ do_tests(int mode, const char *path, int verbose)
 int
 main(int argc, char *argv[])
 {
-  int verbose = 0, ret;
+  int argn = 1, verbose = 0, testmode = 0, ret = 0, apfs;
   char *progname = basename(argv[0]);
   pid_t pid = getpid();
+  const char *cp;
+  char chr;
   char tpath[MAXPATHLEN];
+  struct statfs sfs = {0};
 
-  if (argc > 1 && !strcmp(argv[1], "-v")) verbose = 1;
+  while (argn < argc && argv[argn][0] == '-') {
+    cp = argv[argn];
+    while ((chr = *++cp)) {
+      switch (chr) {
+        case 't': ++testmode; break;
+        case 'v': ++verbose; break;
+      }
+    }
+    ++argn;
+  }
 
   (void) snprintf(tpath, sizeof(tpath), "%s/%s-%u", TEST_TEMP, progname, pid);
 
   if (verbose) printf("%s starting.\n", progname);
 
-  ret = do_tests(0, tpath, verbose);
-  ret |= do_tests(1, tpath, verbose);
+  if (statfs(TEST_TEMP, &sfs)) {
+    printf("  *** statfs() for '" TEST_TEMP "' failed: %s (%d)\n",
+           strerror(errno), errno);
+    ret = 1;
+  } else {
+    if (verbose) printf("  filesystem type is '%s'\n", sfs.f_fstypename);
+    apfs = !strcmp(sfs.f_fstypename, "apfs");
+
+    ret = do_tests(0, tpath, apfs, verbose, testmode);
+    ret |= do_tests(1, tpath, apfs, verbose, testmode);
+  }
 
   printf("%s %s.\n", progname, ret ? "failed" : "passed");
   return ret;
