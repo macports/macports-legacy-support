f5983c8ae575b1b052cb8d7abd9cb3a3d7ce0249
Fred Wright <fw@fwright.net>
2025-07-23 14:19:32 -0700

    runnablearchs.sh: Fix for ppc7400 and arm64e.
    It was erroneously considering ppc7400 to be unrunnable because builds
    for ppc7400 don't define '__ppc7400__'.  This is now fixed.
    
    The arm64e architecture was failing as is probably expected, but with
    an unwanted error message that is now suppressed.
    
    TESTED:
    Now works as expected.
    Tests pass on all platforms.

--- tools/runnablearchs.sh
+++ tools/runnablearchs.sh
@@ -2,24 +2,30 @@
 
 # Script to determine which architectures can build and run on this machine.
 # The list of architectures to test can be specified on the command line;
-# the default is to test all known architectures.  The final output is
+# the default is to test all primary architectures.  The final output is
 # the filtered architecture list.
 #
 # We need to verify that the program can actually run; not just that it built.
 #
 # Some gccs ignore unsupported -arch options, so we need to include a check
-# in the test program to verify the expected architecture.
+# in the test program to verify the expected architecture.  But since there's
+# no '__ppc7400__' preprocessor flag, we need to map 'ppc7400' to 'ppc' while
+# doing this.
 #
 # In addition, some MacPorts clangs segfault when given unsupported -arch
-# options, causing an error message from bash rather than clang.  To get
+# options, causing an error message from the shell rather than clang.  To get
 # around this, we wrap the entire compiler execution in another level of
 # shell, so that we can suppress the error message.
 #
 # Similarly, attempting to run an executable with an unsupported architecture
 # sometimes produces an error message from the shell, so we also wrap this
 # in another shell to allow suppressing the error message.
+#
+# Furthermore, attempts to run an arm64e executable (on arm64) fail with the
+# very nasty signal 9, so we also need to redirect the test program's own
+# stderr to hide that.
 
-ALLARCHS="${@:-ppc ppc64 i386 x86_64 arm64}"
+TESTARCHS="${@:-ppc ppc64 i386 x86_64 arm64}"
 
 if [ "$CC" == "" ]; then CC=cc; fi
 
@@ -27,13 +33,18 @@ TESTBIN="/tmp/testprog-$$"
 TESTSRC="${TESTBIN}.c"
 
 RUNARCHS=""
-for a in $ALLARCHS; do
+for a in $TESTARCHS; do
+  if [ "$a" == "ppc7400" ]; then
+    archflag="ppc"
+  else
+    archflag="$a"
+  fi
 	cat >$TESTSRC <<EOD
 	int
 	main(int argc, char *argv[])
 	{
 		(void) argc; (void) argv;
-	  #ifdef __${a}__
+	  #ifdef __${archflag}__
   		return 0;
   	#else
   	  return 1;
@@ -42,7 +53,7 @@ for a in $ALLARCHS; do
 EOD
   rm -f $TESTBIN
   if sh -c "$CC -arch $a $TESTSRC -o $TESTBIN 2>/dev/null" 2>/dev/null; then
-    if sh -c "$TESTBIN" 2>/dev/null; then
+    if sh -c "$TESTBIN 2>/dev/null" 2>/dev/null; then
       RUNARCHS="$RUNARCHS $a"
     fi
   fi;
