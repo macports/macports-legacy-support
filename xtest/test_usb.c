cd940500e9c760c4861befbb1391619d6ca0a0c7
Fred Wright <fw@fwright.net>
2025-09-23 16:44:54 -0700

    test_usb: Fix "reverse mismatch" failures.
    This test is normally eligible for "reverse SDK mismatch" tests, but
    fails on 10.15 with a <10.12 SDK.  This provides a fix for that case.
    
    It also fails on 26.x+  with a <12.x SDK.  That is also fixed.
    
    TESTED:
    Now passes on all OS/SDK combinations.

--- xtest/test_usb.c
+++ xtest/test_usb.c
@@ -1,5 +1,44 @@
+/* Test that USB.h can be successfully included */
 
-/* Not very exciting but normally this inclusion will fail with GCC 4.5+ pre-10.15 */
+/*
+ * Extra hacks to allow testing with "reverse mismatched" SDK.
+ *
+ * Building for 10.15 with a <10.12 SDK fails due to referencing a
+ * missing definition for uuid_t.  This situation is extremely unlikely
+ * to occur "for real", so we only fix it in the test, just so that we don't
+ * need to exclude this test from the allowable "reverse mismatch" tests.
+ *
+ * Similarly, we provide a missing (empty) definition for building
+ * 26.x+ with a <12.x SDK.
+ */
+
+#if defined(_MACPORTS_LEGACY_MIN_EARLY_SDK_ALLOWED) \
+    && defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
+
+#if __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 101500 \
+    && __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ < 110000
+
+#include <_macports_extras/sdkversion.h>
+
+#if __MPLS_SDK_MAJOR < 101200
+
+#include <sys/_types.h>
+typedef __darwin_uuid_t uuid_t;
+
+#endif  /* __MPLS_SDK_MAJOR < 101200 */
+
+#endif  /* 10.15 target */
+
+#if __ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__ >= 260000
+
+#ifndef __kernel_ptr_semantics
+#define __kernel_ptr_semantics
+#endif
+
+#endif  /* 26.x+ target */
+
+#endif  /* Older SDK allowed and known target */
+
 #include <IOKit/usb/USB.h>
 #include <stdio.h>
 
