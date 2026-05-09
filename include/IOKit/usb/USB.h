2e812f5001c041c43cda1e4444e7df4cc4a43636
Fred Wright <fw@fwright.net>
2026-05-08 21:28:40 -0700

    USB.h: Fix gcc version threshold.
    The issue fixed by our USB.h actually appeared in gcc 4.3, not 4.5 as
    was assumed.
    
    Also reworks the logic to use the correct number of dummy aligns for
    the SDK version (since we now know the SDK version), and expands the
    comments to explain the issue without having to read the full bug
    description.
    
    TESTED:
    Passes the test with all available compilers, including GCC 4.3 and
    4.4.

--- include/IOKit/usb/USB.h
+++ include/IOKit/usb/USB.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021 Evan Miller <emmiller@gmail.com>
+ * Copyright (c) 2026 Frederick H. G. Wright II <fw@fwright.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,20 +15,42 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* pack(1) and align=reset don't mix in some versions of GCC.
- * See: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=50909 */
-#if defined(__GNUC__) && defined(__GNUC_MINOR__) && !defined(__clang__)
-#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5)
-/* 10.4 and up need three invocations */
+/*
+ * pack(1) and align=reset don't mix in some versions of GCC.
+ * See: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=50909
+ *
+ * The basic problem is that since pack(1) is NOP alignment,
+ * GCC 4.3+ ignores it completely, including failing to push an entry
+ * onto the alignment stack.  This causes a "corresponding" align=reset
+ * to malfunction or fail.  This bug has still not been fixed as of GCC 15.
+ *
+ * The workaround is to preload the alignment stack with enough
+ * "NOP but still pushed" alignments to compensate for the failing
+ * pack(1)/reset pairs in the system header.  The number required depends
+ * on the SDK version.
+ *
+ * Although extra dummy pushes are mostly harmless, they may obfuscate
+ * actual overpops in the ensuing code, and since we know the SDK version,
+ * we use the correct number, including none at all in the SDK 10.15+ cases
+ * where pack(1)/reset isn't used.
+ */
+
+/* Do our SDK-related setup */
+#include <_macports_extras/sdkversion.h>
+
+#if __MPLS_SDK_MAJOR < 101500 \
+    && defined(__GNUC__) && defined(__GNUC_MINOR__) && !defined(__clang__) \
+    && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3))
+/* 10.4 SDK and up need at least three invocations */
 #pragma options align=power
 #pragma options align=power
 #pragma options align=power
-/* 10.8 and up need two more */
+/* 10.8 SDK and up need two more */
+#if __MPLS_SDK_MAJOR >= 1080
 #pragma options align=power
 #pragma options align=power
-/* 10.15 and up need none (extras won't hurt so SDK branching is unimplemented) */
-#endif
-#endif
+#endif  /* SDK >= 10.8 */
+#endif  /* SDK < 10.15 and bad GCC */
 
 /* Include the primary system IOKit/usb/USB.h */
 #include_next <IOKit/usb/USB.h>
