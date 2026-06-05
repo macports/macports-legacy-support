ec9e48653add078c1cfef68337df344426bb9de7
Fred Wright <fw@fwright.net>
2026-06-05 15:33:56 -0700

    test_contexts: New test.
    TESTED:
    Detects failures on 10.4 without fix.
    Otherwise passes on all tested platforms.

---
+++ xtest/test_contexts_d.c
@@ -0,0 +1,7 @@
+/*
+ * Version of test_contexts with _DARWIN_C_SOURCE defined.
+ */
+
+#define _DARWIN_C_SOURCE 1
+
+#include "test_contexts.c"
---
+++ xtest/test_contexts_dx.c
@@ -0,0 +1,9 @@
+/*
+ * Version of test_contexts with _XOPEN_SOURCE and _DARWIN_C_SOURCE
+ * defined.
+ */
+
+#define _XOPEN_SOURCE 700
+#define _DARWIN_C_SOURCE 1
+
+#include "test_contexts.c"
