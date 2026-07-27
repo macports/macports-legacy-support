f24ccebff6c9a2239245cf20954441bc9c82510d
Fred Wright <fw@fwright.net>
2026-07-27 12:50:09 -0700

    Add CommonCryptor.h wrapper for 10.4.
    TESTED:
    Now passes new test, including on 10.4.

---
+++ include/CommonCrypto/CommonCryptor.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 Frederick H. G. Wright II <fw@fwright.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * This provides the missing header for 10.4.  On 10.5+, it just passes
+ * the Apple version through.
+ */
+
+/* Do our SDK-related setup */
+#include <_macports_extras/sdkversion.h>
+
+#if __MPLS_SDK_MAJOR < 1050
+
+#include <_macports_extras/tiger_only/CommonCrypto/CommonCryptor.h>
+
+#else  /* __MPLS_SDK_MAJOR >= 1050 */
+
+#include_next <CommonCrypto/CommonCryptor.h>
+
+#endif  /* __MPLS_SDK_MAJOR >= 1050 */
